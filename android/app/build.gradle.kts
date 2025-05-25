446307f59d55802a5f11e448ce087c0b2f0001ed
TheReeds <tomas159357852@gmail.com>
2025-05-25 00:49:29 -0500

    Primer commit: Proyecto Flutter

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.petid.apppetid"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.petid.apppetid"
+        minSdk = 26
+        targetSdk = 35
+        multiDexEnabled = true
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug") // Cambia cuando hagas release real
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color accent = Color(0xFFFF6B6B);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF2D3436);
+  static const Color textSecondary = Color(0xFF636E72);
+  static const Color success = Color(0xFF00B894);
+  static const Color warning = Color(0xFFFDBB2D);
+  static const Color error = Color(0xFFE17055);
+}
